7db3e370cf28560c6ce2008c03b0c075c3a1cd22
mlabarre <michel.labarre@gmail.com>
2024-04-08 13:53:40 +0200

    Fix coingecko error returned catch

--- scripts/quotationFromCoinGecko.js
+++ scripts/quotationFromCoinGecko.js
@@ -20,8 +20,16 @@ let buildQuotationUrl = (ids) => {
 let getQuotationsFromApi = async () => {
     try {
         let symbolList = (await getAllSymbols()).join(",");
-        const response = await fetch(buildQuotationUrl(symbolList));
-        return response.json();
+        let url = buildQuotationUrl(symbolList);
+        fetch(url).then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                console.log(`Error getting coin currency for url ${url}`);
+                console.log(`Error info : ${response.statusText}. ${response.statusMessage}`);
+                return {"errorGecko": true};
+            }
+        });
     } catch (error) {
         console.log(error);
     }
@@ -30,40 +38,42 @@ let getQuotationsFromApi = async () => {
 let update = async () => {
     let updates = 0;
     let currency = config.get('coingecko_currency');
-    let cryptos = await getQuotationsFromApi();  // { id:, name:, eur: }
-    console.log("cryptos",cryptos)
-    let usdtValue = cryptos["tether"][currency];
-    await new MongoHelper().updateUsdtValueInCurrentFiat(usdtValue);
-    for (let cryptoId in cryptos) {
-        let coin = await new MongoHelper().findMyCrypto(cryptoId);
-        if (coin !== null) {
-            let crypto = cryptos[coin.id];
-            coin["last_five_minutes_quotation"] = coin.quotation === null ? 0 : coin.quotation;
-            coin["quotation"] = crypto[currency];
-            coin["quotation_usdt"] = crypto[currency] / usdtValue;
-            coin["quotation_date"] = new Date();
-            coin["last_five_minutes_quotation_date"] = new Date()
-            if (coin["last_one_hour_quotation"] !== undefined) {
-                if ((new Date().getTime() - coin["last_one_hour_quotation_date"].getTime()) >= 3600 * 1000) {
+    // { id:, name:, eur: }
+    let cryptos = await getQuotationsFromApi();
+    if (cryptos.errorGecko !== true) {
+        let usdtValue = cryptos["tether"][currency];
+        await new MongoHelper().updateUsdtValueInCurrentFiat(usdtValue);
+        for (let cryptoId in cryptos) {
+            let coin = await new MongoHelper().findMyCrypto(cryptoId);
+            if (coin !== null) {
+                let crypto = cryptos[coin.id];
+                coin["last_five_minutes_quotation"] = coin.quotation === null ? 0 : coin.quotation;
+                coin["quotation"] = crypto[currency];
+                coin["quotation_usdt"] = crypto[currency] / usdtValue;
+                coin["quotation_date"] = new Date();
+                coin["last_five_minutes_quotation_date"] = new Date()
+                if (coin["last_one_hour_quotation"] !== undefined) {
+                    if ((new Date().getTime() - coin["last_one_hour_quotation_date"].getTime()) >= 3600 * 1000) {
+                        coin["last_one_hour_quotation_date"] = new Date();
+                        coin["last_one_hour_quotation"] = crypto[currency];
+                    }
+                } else {
                     coin["last_one_hour_quotation_date"] = new Date();
                     coin["last_one_hour_quotation"] = crypto[currency];
                 }
-            } else {
-                coin["last_one_hour_quotation_date"] = new Date();
-                coin["last_one_hour_quotation"] = crypto[currency];
-            }
-            if (coin["last_day_quotation"] !== undefined) {
-                if ((new Date().getTime() - coin["last_day_quotation_date"].getTime()) >= 3600 * 1000 * 24) {
+                if (coin["last_day_quotation"] !== undefined) {
+                    if ((new Date().getTime() - coin["last_day_quotation_date"].getTime()) >= 3600 * 1000 * 24) {
+                        coin["last_day_quotation_date"] = new Date();
+                        coin["last_day_quotation"] = crypto[currency];
+                    }
+                } else {
                     coin["last_day_quotation_date"] = new Date();
                     coin["last_day_quotation"] = crypto[currency];
                 }
-            } else {
-                coin["last_day_quotation_date"] = new Date();
-                coin["last_day_quotation"] = crypto[currency];
+                ++updates;
+                console.log(coin)
+                await new MongoHelper().findOneAndReplaceInMyCryptos(coin);
             }
-            ++updates;
-            console.log(coin)
-            await new MongoHelper().findOneAndReplaceInMyCryptos(coin);
         }
     }
     return updates;
