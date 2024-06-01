c095f9f71c211df0466843e109cd8859ea5e012a
mlabarre <michel.labarre@gmail.com>
2024-06-01 14:09:23 +0200

    transalte

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const MongoHelper = require('./scripts/mongoHelper')
 const quotation = require('./scripts/quotationFromCoinGecko')
 const listed = require('./scripts/listedFromCoinGecko')
 const utils = require("./scripts/utils");
+const config = require('config');
 
 let updateQuotation = () => {
+    let msg = config.language === "fr" ? "cotations mises à jour" : "quotations updated"
     quotation.update().then((result) => {
         new MongoHelper().log("quotation",
-            {date: new Date, key: "quotation", info: `${result} quotations updated`})
+            {date: new Date, key: "quotation", info: `${result} ${msg}`})
             .then(() => {
             })
     })
@@ -20,12 +22,14 @@ cron.schedule("*/5 * * * *", () => {
 }, {});
 
 let updateCoins = () => {
+    let msg1 = config.language === "fr" ? "Nouvelles cryptos" : "New cryptos";
+    let msg2 = config.language === "fr" ? "cryptos mises à jour" : "cryptos updated";
     listed.update().then((result) => {
         new MongoHelper().log("coins",
-            {date: new Date, key: "coins", info: `${result.news} new cryptos, ${result.updates} cryptos updated`})
+            {date: new Date, key: "coins", info: `${result.news} ${msg1.toLowerCase()}, ${result.updates} ${msg2}`})
             .then(() => {
                 if (result.newCoins.length > 0) {
-                    utils.sendNotification(utils.formatNewCoins(result.newCoins), 'New cryptos')
+                    utils.sendNotification(utils.formatNewCoins(result.newCoins), msg1)
                         .then((res) => {
                     });
                 }
